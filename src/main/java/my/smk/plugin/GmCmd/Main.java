f974eace57b98c76c5b00bebc508cf20ccf9563a
smk7758 <maxkovko@yahoo.co.jp>
2016-12-04 16:08:39 +0900

    GmCmd-0.0.2
    従来の切り替えコマンドに加えて、gm0~3のコマンドを追加しました。

---
+++ src/main/java/my/smk/plugin/GmCmd/Main.java
@@ -0,0 +1,78 @@
+package my.smk.plugin.GmCmd;
+
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	public final Logger log = Logger.getLogger("Minecraft");
+	public String PluginName = getDescription().getName();
+	public String ChatPrefix = ChatColor.YELLOW + "[" + ChatColor.GREEN + PluginName + ChatColor.YELLOW + "] " + ChatColor.WHITE;
+	public String ErroPrefix = ChatColor.WHITE + "[" + ChatColor.RED + PluginName + ChatColor.WHITE + "] " + ChatColor.WHITE;
+
+	public void onEnable() {
+	}
+
+	public void onDisable() {
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("gm")) {// gm”½“]cmd
+			if (sender instanceof Player) {
+				if ((sender.hasPermission("GmCmd")) || (sender.isOp())) {
+					Player player = (Player) sender;
+					GameMode gm = player.getGameMode();
+					if (gm != null) {
+						if (gm == GameMode.CREATIVE) {
+							player.setGameMode(GameMode.SURVIVAL);
+							return true;
+						} else if (gm == GameMode.SURVIVAL) {
+							player.setGameMode(GameMode.CREATIVE);
+							return true;
+						} else {
+							player.setGameMode(GameMode.CREATIVE);
+							return true;
+						}
+					}
+				} else {
+					sender.sendMessage(this.ErroPrefix + "You don't have permission to use gm command!");
+				}
+			} else {
+				sender.sendMessage("You have to use it in the game.(Player)");
+			}
+		}
+		if (cmd.getName().equalsIgnoreCase("gm0") || cmd.getName().equalsIgnoreCase("gm1") || cmd.getName().equalsIgnoreCase("gm2") || cmd.getName().equalsIgnoreCase("gm3")) {
+			if (sender instanceof Player) {
+				if ((sender.hasPermission("GmCmd")) || (sender.isOp())) {
+					Player player = (Player)sender;
+					GameMode gamemode_type = player.getGameMode();
+					if (cmd.getName().equalsIgnoreCase("gm0")) {
+						gamemode_type = GameMode.SURVIVAL;
+					}
+					if (cmd.getName().equalsIgnoreCase("gm1")) {
+						gamemode_type = GameMode.CREATIVE;
+					}
+					if (cmd.getName().equalsIgnoreCase("gm2")) {
+						gamemode_type = GameMode.ADVENTURE;
+					}
+					if (cmd.getName().equalsIgnoreCase("gm3")) {
+						gamemode_type = GameMode.SPECTATOR;
+					}
+					player.setGameMode(gamemode_type);
+					return true;
+				} else {
+					sender.sendMessage(this.ErroPrefix + "You don't have permission to use gm command!");
+				}
+			} else {
+				sender.sendMessage("You have to use it in the game.(Player)");
+			}
+		}
+		return false;
+	}
+}
